fdbedf356e5135e9d41d9d067ed72615bd46a781
cess-uyi <ebhoayesuccess@gmail.com>
2021-11-12 11:04:25 +0100

    first commit

---
+++ handlers/validator/invoiceSubheading.js
@@ -0,0 +1,39 @@
+const { body } = require("express-validator");
+const userModel = require("../../models/user");
+const invoiceSubheadingModel = require("../../models/invoiceSubheading");
+
+exports.CREATE = [
+  body("clientId")
+    .notEmpty()
+    .trim()
+    .isMongoId()
+    .custom((value, { req }) => {
+      return userModel
+        .findOne({ _id: value, userType: req.query.type })
+        .then((result) => {
+          if (!result) {
+            return Promise.reject("resource not found");
+          }
+        });
+    }),
+  body("replaceSalesOrderNo").notEmpty().isString().trim().toLowerCase(),
+  body("replaceInvoiceNo").notEmpty().isString().trim().toLowerCase(),
+];
+
+exports.UPDATE = [
+  body("clientId")
+    .notEmpty()
+    .trim()
+    .isMongoId()
+    .custom((value, { req }) => {
+      return userModel
+        .findOne({ _id: value, userType: "business" })
+        .then((result) => {
+          if (!result) {
+            return Promise.reject("resource not found");
+          }
+        });
+    }),
+  body("replaceSalesOrderNo").notEmpty().isString().trim().toLowerCase(),
+  body("replaceInvoiceNo").notEmpty().isString().trim().toLowerCase(),
+];
